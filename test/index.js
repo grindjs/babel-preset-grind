2bfb0fb1d8c08bc7571bea1fb86089630a97e0ac
shnhrrsn <shnhrrsn@gmail.com>
2020-08-03 00:25:30 -0400

    Update tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-import test from 'ava'
-import { transform } from "@babel/core"
-import grindPresent from '../'
+const test = require('ava')
+const { transform } = require('@babel/core')
+const grindPresent = require('../')
 
 test('exposes function', t => {
 	t.is(typeof grindPresent, 'function')
@@ -18,8 +18,8 @@ test('exposes a separate list of plugins', t => {
 test('options', t => {
 	grindPresent(null, {
 		'import-auto-name': {
-			'autoresolve': true
-		}
+			autoresolve: true,
+		},
 	})
 
 	t.pass()
@@ -28,27 +28,30 @@ test('options', t => {
 test('object spread', t => {
 	t.is(
 		transform('const a = { ...obj, a: true }', grindPresent()).code,
-		'"use strict";\n\nconst a = { ...obj,\n  a: true\n};'
+		'"use strict";\n\nconst a = { ...obj,\n  a: true\n};',
 	)
 })
 
 test('class properties', t => {
 	t.is(
 		transform('class test {\nstatic a = 1;\nb = 2;\n}', grindPresent()).code,
-		'"use strict";\n\nclass test {\n  constructor() {\n    this.b = 2;\n  }\n\n}\n\ntest.a = 1;'
+		'"use strict";\n\nclass test {\n  constructor() {\n    this.b = 2;\n  }\n\n}\n\ntest.a = 1;',
 	)
 })
 
 test('isNil', t => {
 	t.is(
 		transform('something.isNil', grindPresent()).code,
-		'"use strict";\n\nfunction _isnil(val) { return val === null || typeof val === \'undefined\'; }\n\n_isnil(something);'
+		'"use strict";\n\nfunction _isnil(val) { return val === null || typeof val === \'undefined\'; }\n\n_isnil(something);',
 	)
 })
 
 test('import auto name', t => {
 	t.is(
-		transform('import \'App/Controllers/SomeController\'\nconst c = new SomeController', grindPresent()).code,
-		'"use strict";\n\nvar _SomeController = require("App/Controllers/SomeController");\n\nconst c = new _SomeController.SomeController();'
+		transform(
+			"import 'App/Controllers/SomeController'\nconst c = new SomeController",
+			grindPresent(),
+		).code,
+		'"use strict";\n\nvar _SomeController = require("App/Controllers/SomeController");\n\nconst c = new _SomeController.SomeController();',
 	)
 })
